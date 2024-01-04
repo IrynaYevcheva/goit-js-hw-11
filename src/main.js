93dfa6397441db91c5e1969868f5765f9f30ac8e
IrynaYevcheva <irenmikos@gmail.com>
2024-01-05 00:44:25 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,45 +5,54 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#form');
-const formInput = document.querySelector('#formInput');
 const gallery = document.querySelector('#gallery');
 const loadingMessage = document.querySelector('.loader');
 
+form.addEventListener('submit', fetchImg);
+
 let lightbox = new SimpleLightbox('#gallery a', {
   showCounter: false,
-});
-
-iziToast.settings({
-  close: true,
-  closeOnClick: false,
-  message:
-    'Sorry, there are no images matching your search query. Please try again!',
-  messageColor: 'white',
-  timeout: 4000,
-  transitionIn: 'flipInX',
-  transitionOut: 'flipOutX',
-  position: 'topRight',
-  backgroundColor: 'red',
+  captionsData: 'alt',
+  captionDelay: 200,
 });
 
 function onError() {
   gallery.innerHTML = '';
-  iziToast.show();
+  iziToast.show({
+    close: true,
+    closeOnClick: false,
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+    messageColor: 'white',
+    timeout: 4000,
+    transitionIn: 'flipInX',
+    transitionOut: 'flipOutX',
+    position: 'topRight',
+    backgroundColor: '#EF4040',
+  });
 }
 
-form.addEventListener('submit', fetchImg);
+function hideLoader() {
+  return loadingMessage.classList.add('hidden');
+}
 
 function fetchImg(event) {
-  loadingMessage.classList.remove('hidden');
   event.preventDefault();
+  loadingMessage.classList.remove('hidden');
+  gallery.innerHTML = '';
+  const search = event.currentTarget.elements.inputToSearch.value.trim();
+  getImg(search);
+  event.currentTarget.reset();
+}
+
+function getImg(search) {
   const searchParams = new URLSearchParams({
     key: '41535570-7b1028e1c6f1b041bb0744cc1',
-    q: formInput.value,
+    q: search,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   });
-
   fetch(`https://pixabay.com/api/?${searchParams}`)
     .then(response => {
       if (!response.ok) {
@@ -52,41 +61,37 @@ function fetchImg(event) {
       return response.json();
     })
     .then(images => {
-      setTimeout(() => {
-        loadingMessage.classList.add('hidden');
-        if (images.hits.length === 0) {
-          return onError();
-        }
-        renderImages(images.hits);
-      }, 1000);
+      if (images.hits.length === 0) {
+        onError();
+      }
+      renderImages(images);
+      hideLoader();
+      lightbox.refresh();
     })
-    .catch(error => console.log(error))
+    .catch(error => {
+      iziToast.error();
+      hideLoader();
+    })
     .finally(() => {
-      form.reset();
+      hideLoader();
     });
 }
 
 function renderImages(images) {
-  gallery.innerHTML = images.reduce(
-    (
-      html,
-      { webformatURL, largeImageURL, tags, likes, views, comments, downloads }
-    ) =>
-      html +
-      `
-      <li class="gallery-item">
-        <a href="${largeImageURL}">
-          <img class="gallery-img" src="${webformatURL}" alt="${tags}" />
+  return (gallery.innerHTML = images.hits
+    .map(
+      img =>
+        `<li class="gallery-item">
+        <a href="${img.largeImageURL}">
+          <img class="gallery-img" src="${img.webformatURL}" alt="${img.tags}" />
         </a>
         <div class="image-info">
-          <div>Likes:<span>${likes}</span></div>
-          <div>Views:<span>${views}</span></div>
-          <div>Comments:<span>${comments}</span></div>
-          <div>Downloads:<span>${downloads}</span></div>
+          <div>Likes:<span>${img.likes}</span></div>
+          <div>Views:<span>${img.views}</span></div>
+          <div>Comments:<span>${img.comments}</span></div>
+          <div>Downloads:<span>${img.downloads}</span></div>
         </div>
-      </li>
-      `,
-    ''
-  );
-  lightbox.refresh();
+      </li>`
+    )
+    .join(''));
 }
