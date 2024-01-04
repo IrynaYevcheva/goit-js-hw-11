ccfba26f0cb8cf908393080f08036b84e3f45423
IrynaYevcheva <140852372+IrynaYevcheva@users.noreply.github.com>
2024-01-04 22:44:41 +0000

    Deploying to gh-pages from @ IrynaYevcheva/goit-js-hw-11@93dfa6397441db91c5e1969868f5765f9f30ac8e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,12 @@
-import{S as m,i as l}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const c=document.querySelector("#form"),h=document.querySelector("#formInput"),u=document.querySelector("#gallery"),a=document.querySelector(".loader");let g=new m("#gallery a",{showCounter:!1});l.settings({close:!0,closeOnClick:!1,message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"white",timeout:4e3,transitionIn:"flipInX",transitionOut:"flipOutX",position:"topRight",backgroundColor:"red"});function p(){u.innerHTML="",l.show()}c.addEventListener("submit",y);function y(i){a.classList.remove("hidden"),i.preventDefault();const o=new URLSearchParams({key:"41535570-7b1028e1c6f1b041bb0744cc1",q:h.value,image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`https://pixabay.com/api/?${o}`).then(r=>{if(!r.ok)throw new Error(r.status);return r.json()}).then(r=>{setTimeout(()=>{if(a.classList.add("hidden"),r.hits.length===0)return p();v(r.hits)},1e3)}).catch(r=>console.log(r)).finally(()=>{c.reset()})}function v(i){u.innerHTML=i.reduce((o,{webformatURL:r,largeImageURL:s,tags:e,likes:t,views:n,comments:d,downloads:f})=>o+`
-      <li class="gallery-item">
-        <a href="${s}">
-          <img class="gallery-img" src="${r}" alt="${e}" />
+import{S as d,i as c}from"./assets/vendor-46aac873.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))l(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function o(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerpolicy&&(r.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?r.credentials="include":t.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(t){if(t.ep)return;t.ep=!0;const r=o(t);fetch(t.href,r)}})();const f=document.querySelector("#form"),a=document.querySelector("#gallery"),u=document.querySelector(".loader");f.addEventListener("submit",p);let m=new d("#gallery a",{showCounter:!1,captionsData:"alt",captionDelay:200});function h(){a.innerHTML="",c.show({close:!0,closeOnClick:!1,message:"Sorry, there are no images matching your search query. Please try again!",messageColor:"white",timeout:4e3,transitionIn:"flipInX",transitionOut:"flipOutX",position:"topRight",backgroundColor:"#EF4040"})}function s(){return u.classList.add("hidden")}function p(n){n.preventDefault(),u.classList.remove("hidden"),a.innerHTML="";const e=n.currentTarget.elements.inputToSearch.value.trim();g(e),n.currentTarget.reset()}function g(n){const e=new URLSearchParams({key:"41535570-7b1028e1c6f1b041bb0744cc1",q:n,image_type:"photo",orientation:"horizontal",safesearch:!0});fetch(`https://pixabay.com/api/?${e}`).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()}).then(o=>{o.hits.length===0&&h(),y(o),s(),m.refresh()}).catch(o=>{c.error(),s()}).finally(()=>{s()})}function y(n){return a.innerHTML=n.hits.map(e=>`<li class="gallery-item">
+        <a href="${e.largeImageURL}">
+          <img class="gallery-img" src="${e.webformatURL}" alt="${e.tags}" />
         </a>
         <div class="image-info">
-          <div>Likes:<span>${t}</span></div>
-          <div>Views:<span>${n}</span></div>
-          <div>Comments:<span>${d}</span></div>
-          <div>Downloads:<span>${f}</span></div>
+          <div>Likes:<span>${e.likes}</span></div>
+          <div>Views:<span>${e.views}</span></div>
+          <div>Comments:<span>${e.comments}</span></div>
+          <div>Downloads:<span>${e.downloads}</span></div>
         </div>
-      </li>
-      `,""),g.refresh()}
+      </li>`).join("")}
 //# sourceMappingURL=commonHelpers.js.map
